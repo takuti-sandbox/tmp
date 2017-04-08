3969f3a25e6fb7f4f129adb6b3bd204b28e6c448
Takuya Kitazawa <k.takuti@gmail.com>
2017-04-08 10:02:10 +0900

    Add Recommendation.jl MPR sample

---
+++ julia/mpr_sample.jl
@@ -0,0 +1,28 @@
+using Recommendation
+
+R = [0 1 1;
+     1 0 1;
+     0 1 0;
+     1 0 1;
+     1 0 0]
+
+Predicted = [0.6 0.8 0.31;
+             0.7 0.2 0.25;
+             0.5 0.9 0.46;
+             0.3 0.1 0.81;
+             0.9 0.4 0.5]
+
+n_item, n_user = size(R)
+
+MPRs = []
+for user in 1:n_user
+    truth = find(r -> r == 1, R[:, user])
+    rank = sortperm(Predicted[:, user], rev=true)
+
+    mpr = measure(MPR(), truth, rank)
+    push!(MPRs, mpr)
+end
+
+println(MPRs)
+println(mean(MPRs))
+
