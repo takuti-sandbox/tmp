67f851a49d498f5a1d6fdcef30c58f2ca6d16c34
Takuya Kitazawa <k.takuti@gmail.com>
2017-04-08 10:11:23 +0900

    Compute MPR only for testing samples

--- julia/mpr_sample.jl
+++ julia/mpr_sample.jl
@@ -12,12 +12,20 @@ Predicted = [0.6 0.8 0.31;
              0.3 0.1 0.81;
              0.9 0.4 0.5]
 
+is_test = [0 0 0;
+           1 0 1;
+           0 1 0;
+           0 0 1;
+           1 0 0]
+
 n_item, n_user = size(R)
 
 MPRs = []
 for user in 1:n_user
-    truth = find(r -> r == 1, R[:, user])
-    rank = sortperm(Predicted[:, user], rev=true)
+    test_indices = find(flg -> flg == 1, is_test[:, user])
+
+    truth = find(r -> r == 1, R[test_indices, user])
+    rank = sortperm(Predicted[test_indices, user], rev=true)
 
     mpr = measure(MPR(), truth, rank)
     push!(MPRs, mpr)
