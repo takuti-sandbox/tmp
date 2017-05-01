0b60c0352a006783dbebc57f6b2115c7c78e9d22
Takuya Kitazawa <k.takuti@gmail.com>
2017-05-01 17:53:42 +0900

    Improve sklearn + LDA example

--- python/lda/news20.py
+++ python/lda/news20.py
@@ -1,37 +1,47 @@
-from sklearn.decomposition import LatentDirichletAllocation
 import numpy as np
 import time
 
-
-path = '/Users/kitazawa/data/news20-multiclass'
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.decomposition import LatentDirichletAllocation
 
 
 def main():
-    k = 20
-    n_features = 62061
+    newsgroups = fetch_20newsgroups(shuffle=True, random_state=101, subset='train', remove=('headers', 'footers', 'quotes'))
 
-    labels = [0] * k
-    X = np.zeros((k, n_features))
+    vectorizer = CountVectorizer(max_df=0.9, min_df=2, max_features=1000, stop_words='english')
+    vectors = vectorizer.fit_transform(newsgroups.data)
+    vocab = vectorizer.get_feature_names()
+
+    with open('news20-vectorized', 'w') as f:
+        prev_row = 0
+        tokens = []
+        rows, cols = vectors.nonzero()
+        for row, col in zip(rows, cols):
+            if row != prev_row:
+                f.write(str(newsgroups.target[prev_row]) + ' ' + ' '.join(tokens) + '\n')
+                prev_row = row
+                tokens = []
+            tokens.append(vocab[col] + ':' + str(vectors[row, col]))
+
+    n_samples, n_features = vectors.shape
+    print(n_samples, n_features)
+
+    K = 20
 
     t0 = time.time()
 
-    with open(path) as f:
-        for l in f.readlines():
-            line = l.rstrip().split(' ')
-            idx = int(line[0]) - 1
-            if labels[idx] == 0:
-                labels[idx] = 1
-                for f in line[1:]:
-                    fi, val = f.split(':')
-                    X[idx, int(fi) - 1] = float(val)
-
-    # print(np.count_nonzero(X))  # 1497
-
-    lda = LatentDirichletAllocation(n_topics=20, learning_decay=.8, learning_offset=80, max_iter=30, total_samples=2000,
-                                    batch_size=2, perp_tol=1e-1, mean_change_tol=1e-3)
-    lda.fit(X)
+    lda = LatentDirichletAllocation(n_topics=K, learning_decay=.8, learning_offset=40, max_iter=5, total_samples=n_samples,
+                                    batch_size=128, perp_tol=1e-1, mean_change_tol=1e-3)
+    lda.fit(vectors)
 
     print('Elapsed time: %d sec for 30 iters' % (time.time() - t0))  # ~ 15 sec
+
+    for k in range(K):
+        print('=== Topic %2d ===' % k)
+        top5words_indices = np.argsort(lda.components_[k])[::-1][:5]
+        for i in top5words_indices:
+            print(vocab[i])
 
 
 if __name__ == '__main__':
