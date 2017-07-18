1d628a9ceecb03e583f5aceeb30ab4d2b0a29298
Takuya Kitazawa <k.takuti@gmail.com>
2017-07-18 17:49:22 +0900

    Override global function used in a class

--- python/class/c2.py
+++ python/class/c2.py
@@ -1,7 +1,12 @@
+import c1
 from c1 import Foo
 
 
 def func():
+    return '222'
+
+
+def func2():
     return '222'
 
 
@@ -13,3 +18,6 @@ class Bar(Foo):
 
 print('Foo: %s' % Foo())
 print('Bar: %s' % Bar())
+
+c1.func = func2
+print('Foo: %s' % Foo())
