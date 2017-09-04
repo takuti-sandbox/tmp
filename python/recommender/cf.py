722e5ea529a6c121d14e430ccc01fcab320603ca
Takuya Kitazawa <k.takuti@gmail.com>
2017-09-04 22:10:24 +0900

    Compute full user-user similarities

--- python/recommender/cf.py
+++ python/recommender/cf.py
@@ -6,9 +6,20 @@ def similarity(x, y):
     return np.inner(x, y) / (ln.norm(x, ord=2) * ln.norm(y, ord=2))
 
 
-user_a = np.array([5, 0, 1, 1, 0, 2])
-user_b = np.array([0, 2, 0, 4, 0, 4])
-user_c = np.array([4, 5, 0, 1, 1, 2])
+# 5 users * 6 items
+A = np.array([[5, 0, 1, 1, 0, 2],
+              [0, 2, 0, 4, 0, 4],
+              [4, 5, 0, 1, 1, 2],
+              [0, 0, 3, 5, 2, 0],
+              [2, 0, 1, 0, 4, 4]])
 
-print(similarity(user_a, user_b))  # 0.359210604054
-print(similarity(user_a, user_c))  # 0.654953146328
+# user-user similarity matrix
+n_user = A.shape[0]
+S = np.zeros((n_user, n_user))
+
+for i in range(n_user):
+    S[i, i] = 1.
+    for j in range(i + 1, n_user):
+        S[i, j] = S[j, i] = similarity(A[i], A[j])
+
+print(S)
