d6287e10c5c304383e9c5b3a9b139f58ee3add23
Takuya Kitazawa <k.takuti@gmail.com>
2017-09-12 22:00:46 +0900

    Update comments

--- python/pytorch/autograd.py
+++ python/pytorch/autograd.py
@@ -5,12 +5,21 @@ import torch
 from torch.autograd import Variable
 
 
+# [x1, x2; x3, x4] = [1, 1; 1, 1]
 x = Variable(torch.ones(2, 2), requires_grad=True)
+
+# [y1, y2; y3, y4] = [x+2, x+2; x+2, x+2]
 y = x + 2
 
-z = y * y * 3  # 3 * (x + 2)^2
-out = z.mean()  # single from 3 * (1 + 2)^2
+# zi = 3 * (xi + 2)^2
+z = y * y * 3
 
+# out = 1/4 * (z1 + z2 + z3 + z4)
+#     = 1/4 * (3 * (x1 + 2)^2 + 3 * (x2 + 2)^2 + 3 * (x3 + 2)^2 + 3 * (x4 + 2)^2)
+out = z.mean()
+
+# d(out)
 out.backward()
 
+# d(out) / d(xi) = 1/4 * 2 * 3 * (xi + 2) = 3/2 * (xi + 2) = 4.5 (because xi = 1)
 print(x.grad)
