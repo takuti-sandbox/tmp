b9c2836c253c070423167b3bac01c931eebcbcb3
Takuya Kitazawa <k.takuti@gmail.com>
2017-09-18 11:26:33 +0900

    Refactor concurrent is_prime()

--- python/concurrent/prime.py
+++ python/concurrent/prime.py
@@ -1,71 +1,45 @@
+import concurrent.futures
 import math
-from concurrent import futures
 
-
-def is_prime(n):
-    """
-    >>> is_prime(41)
-    True
-    >>> is_prime(42)
-    False
-    """
-    if n == 1:
-        return False
-    elif n == 2:
-        return True
-
-    for i in range(2, (int(math.sqrt(n)) + 1) + 1):
-        if n % i == 0:
-            return False
-
-    return True
-
-
-values = [
+PRIMES = [
     112272535095293,
     112582705942171,
     112272535095293,
     115280095190773,
     115797848077099,
-    1099726899285419,
-]
+    1099726899285419]
 
 
-def task_single():
-    for value in values:
-        print(value, is_prime(value))
+def is_prime(n):
+    if n % 2 == 0:
+        return False
 
-
-def task_multithread():
-    with futures.ThreadPoolExecutor() as executor:
-        future_to_value = {executor.submit(is_prime, value): value for value in values}
-        for future in futures.as_completed(future_to_value):
-            print(future_to_value[future], future.result())
-
-
-def task_multiprocess():
-    with futures.ProcessPoolExecutor() as executor:
-        future_to_value = {executor.submit(is_prime, value): value for value in values}
-        for future in futures.as_completed(future_to_value):
-            print(future_to_value[future], future.result())
+    sqrt_n = int(math.floor(math.sqrt(n)))
+    for i in range(3, sqrt_n + 1, 2):
+        if n % i == 0:
+            return False
+    return True
 
 
 if __name__ == '__main__':
     import time
-    import sys
-
-    if len(sys.argv) < 2:
-        exit()
 
     start = time.time()
-
-    if sys.argv[1] == 'single':
-        task_single()
-    elif sys.argv[1] == 'multithread':
-        task_multithread()
-    elif sys.argv[1] == 'multiprocess':
-        task_multiprocess()
-
+    for number, prime in zip(PRIMES, map(is_prime, PRIMES)):
+        print('%d is prime: %s' % (number, prime))
     end = time.time()
+    print('Single:        elapsed time = {}'.format(end - start))
 
-    print('{}: elapsed time = {}'.format(sys.argv[1], end - start))
+    start = time.time()
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        for number, prime in zip(PRIMES, executor.map(is_prime, PRIMES)):
+            print('%d is prime: %s' % (number, prime))
+    end = time.time()
+    print('Multi-thread:  elapsed time = {}'.format(end - start))
+
+    start = time.time()
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        for number, prime in zip(PRIMES, executor.map(is_prime, PRIMES)):
+            print('%d is prime: %s' % (number, prime))
+    end = time.time()
+    print('Multi-process: elapsed time = {}'.format(end - start))
