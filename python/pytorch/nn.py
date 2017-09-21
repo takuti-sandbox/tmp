43f7b26131e9f32fb889a8c3540a44618f82b23f
Takuya Kitazawa <k.takuti@gmail.com>
2017-09-21 23:47:30 +0900

    Refactor

--- python/pytorch/nn.py
+++ python/pytorch/nn.py
@@ -5,12 +5,18 @@ import torch
 import torch.autograd as autograd
 import torch.nn as nn
 
-
 torch.manual_seed(1)
 
-lin = nn.Linear(5, 3)
-data = autograd.Variable(torch.randn(2, 5))
-print(data)  # X (2x5)
-print(lin.weight)  # A (5x3)
-print(lin.bias)  # b (3x1)
-print(lin(data))  # AX + b (2x3)
+
+def affine_map():
+    lin = nn.Linear(5, 3)
+    data = autograd.Variable(torch.randn(2, 5))
+    print(data)  # X (2x5)
+    # `lin.weight`: A (5x3)
+    # `lin.bias`: b (3x1)
+    print(lin(data))  # AX + b (2x3)
+
+
+if __name__ == '__main__':
+    print('Affine map')
+    affine_map()
