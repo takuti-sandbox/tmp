069b4d5e2411be45796206c385946372795cfe91
Takuya Kitazawa <k.takuti@gmail.com>
2017-10-14 20:21:35 +0900

    Refactor with convergence check

--- python/pytorch/mf.py
+++ python/pytorch/mf.py
@@ -1,17 +1,32 @@
+"""Matrix Factorization using PyTorch
+
+http://blog.ethanrosenthal.com/2017/06/20/matrix-factorization-in-pytorch/
+"""
+
+import random
 import numpy as np
 import torch
+from torch import autograd, nn, optim
 
 
-class MatrixFactorization(torch.nn.Module):
+class MatrixFactorization(nn.Module):
 
     def __init__(self, n_user, n_item, k=20):
         super().__init__()
 
-        self.user_factors = torch.nn.Embedding(n_user, k, sparse=True)
-        self.item_factors = torch.nn.Embedding(n_item, k, sparse=True)
+        self.user_factors = nn.Embedding(n_user, k, sparse=True)
+        self.item_factors = nn.Embedding(n_item, k, sparse=True)
 
     def forward(self, user, item):
         return (self.user_factors(user) * self.item_factors(item)).sum(1)
+
+
+def as_long_tensor(val):
+    return torch.LongTensor([np.long(val)])
+
+
+def as_float_tensor(val):
+    return torch.FloatTensor([np.long(val)])
 
 
 def main():
@@ -24,39 +39,44 @@ def main():
     n_user, n_item = R.shape
 
     model = MatrixFactorization(n_user, n_item)
-    loss_function = torch.nn.MSELoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=1e-6)
+    loss_function = nn.MSELoss()
+    optimizer = optim.SGD(model.parameters(), lr=1e-6)
 
-    for each in range(3):
-        for user in range(n_user):
-            for item in range(n_item):
-                if R[user, item] == 0.:
-                    continue
-
-                model.zero_grad()
-
-                user_variable = torch.autograd.Variable(torch.LongTensor([np.long(user)]))
-                item_variable = torch.autograd.Variable(torch.LongTensor([np.long(item)]))
-                rating = torch.autograd.Variable(torch.FloatTensor([np.float(R[user, item])]))
-
-                prediction = model(user_variable, item_variable)
-
-                loss = loss_function(prediction, rating)
-
-                loss.backward()
-
-                optimizer.step()
-
-    for user in range(n_user):
-        for item in range(n_item):
-            if R[user, item] == 0.:
+    last_accum_loss = float('inf')
+    indices = list(zip(range(n_user), range(n_item)))
+    while True:
+        accum_loss = 0
+        random.shuffle(indices)
+        for u, i in indices:
+            r = R[u, i]
+            if r == 0.:
                 continue
 
-            user_variable = torch.autograd.Variable(torch.LongTensor([np.long(user)]))
-            item_variable = torch.autograd.Variable(torch.LongTensor([np.long(item)]))
+            model.zero_grad()
 
-            prediction = model(user_variable, item_variable)
-            print(user, item, R[user, item], prediction)
+            user = autograd.Variable(as_long_tensor(u))
+            item = autograd.Variable(as_long_tensor(i))
+            rating = autograd.Variable(as_float_tensor(r))
+
+            prediction = model(user, item)
+
+            loss = loss_function(prediction, rating)
+            accum_loss += loss.data[0]
+
+            loss.backward()
+
+            optimizer.step()
+
+        if abs(accum_loss - last_accum_loss) < 1e-3:
+            break
+        last_accum_loss = accum_loss
+
+    for u in range(n_user):
+        for i in range(n_item):
+            user = autograd.Variable(as_long_tensor(u))
+            item = autograd.Variable(as_long_tensor(i))
+            prediction = model(user, item)
+            print(u, i, R[u, i], prediction.data[0])
 
 
 if __name__ == '__main__':
