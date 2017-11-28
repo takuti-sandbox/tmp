5677dce2fa7ebad912babf1212c82a5f9727ce01
Takuya Kitazawa <k.takuti@gmail.com>
2017-11-28 11:37:18 +0900

    Support bi-gram words along with simplification of tokenizer

--- python/nlp/tokenize_en.py
+++ python/nlp/tokenize_en.py
@@ -1,11 +1,16 @@
-from nltk import pos_tag_sents, sent_tokenize, word_tokenize
-from gensim.utils import to_unicode, deaccent
+from gensim.utils import to_unicode, deaccent, PAT_ALPHABETIC
+
+
+# uni- or bi-gram
+VALID_WORDS = set(['machine', 'learning', 'machine learning'])
 
 
 def tokenize_en(content, token_min_len, token_max_len, lower):
     """
+    Note that this function does not use `token_min_len`, `token_max_len` and `lower`.
+
     >>> tokenize_en('Machine learning is fun!', 2, 15, True)
-    ['machine', 'learning', 'fun']
+    ['machine', 'learning', 'machine learning']
     """
     def get_tokens(text):
         text = to_unicode(text, encoding='utf8', errors='ignore')
@@ -14,17 +19,20 @@ def tokenize_en(content, token_min_len, token_max_len, lower):
         # normalize unicode (i.e., remove accentuation)
         text = deaccent(text)
 
-        token_sents = [word_tokenize(sent) for sent in sent_tokenize(text)]
+        bi = []
+        for match in PAT_ALPHABETIC.finditer(text):
+            uni = match.group()
+            yield uni
 
-        # only Noun is acceptable: https://pythonprogramming.net/natural-language-toolkit-nltk-part-speech-tagging/
-        return [token
-                for pos_tag_sent in pos_tag_sents(token_sents)
-                for token, pos in pos_tag_sent
-                if pos == 'NN' or pos == 'NNS' or pos == 'NNP' or pos == 'NNPS']
+            bi.append(uni)
+            if len(bi) == 1:
+                continue
+            yield ' '.join(bi)
+            del bi[0]
 
     return [
         to_unicode(token) for token in get_tokens(content)
-        if token_min_len <= len(token) <= token_max_len and not token.startswith('_')
+        if token in VALID_WORDS
     ]
 
 
