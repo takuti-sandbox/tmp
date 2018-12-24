2753ff9f03df4e415a6ae6cd3eed08936ed5b02d
Takuya Kitazawa <k.takuti@gmail.com>
2018-12-25 08:21:31 +0900

    Replace `to_utf8(cast(id as varchar))` with `to_bigendian_64()`
    Based on discussion at: https://github.com/prestodb/presto/pull/2397

--- misc/digdag/energy-benchmarking/queries/shuffle.sql
+++ misc/digdag/energy-benchmarking/queries/shuffle.sql
@@ -1,5 +1,5 @@
 select
   *,
-  abs(from_big_endian_64(xxhash64(to_utf8(cast(id as varchar)))) % 100) as random_flag
+  abs(from_big_endian_64(xxhash64(to_big_endian_64(id))) % 100) as random_flag
 from
   vectors
